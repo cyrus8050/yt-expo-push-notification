f20a751e433d3bb975ad2dfe54a1102968a978f2
Cyrus Chan <cyruschan@hsu.edu.hk>
2020-10-29 15:59:15 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useRef,useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 export default function App() {
+  // const notificationListener = useRef();
+  // const responseListener = useRef();
+  
+  // const [notification, setNotification] = useState(false);
+  useEffect(() => {
+    registerForPushNotification().then(token=>console.log(token));
+    // notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+    //   console.log(notification);
+    // });
+    // responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+    //   console.log(response);
+    // });
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+
+  async function registerForPushNotification(){
+    const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if (status != 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      // finalStatus = status;
+    }
+    if (status !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    return token
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
